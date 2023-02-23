6dd61e513446a1e1d0ef60e88390444f519994e0
李展旗 <lizhanqi@inke.cn>
2023-02-23 19:33:21 +0800

    简单写写

---
+++ app/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"go-task/conf"
+	"go-task/core"
+)
+
+// 在main中，只关心这个app是否成功启动，不关心app内是什么。
+//所以main里的逻辑：加载配置、初始化app、启动app、监听信号、收到停止信号或者reload信号时，分别触发app的停止和reload、退出。
+
+func main() {
+	// 加载配置
+	c, err := conf.ReadConfig()
+	if err != nil {
+		log.Fatal("读取配置失败, err:", err)
+	}
+
+	// 初始化app
+	ser, err := core.NewService(c)
+	if err != nil {
+		log.Fatal("初始化服务失败, err:", err)
+	}
+	log.Println("服务已启动")
+
+	// 启动app
+	defer func() {
+		// 终止app
+		ser.Stop()
+		log.Println("服务已退出")
+	}()
+
+	// 监听信号
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	for {
+		s := <-sigChan
+		log.Printf("系统信号:%s\n", s.String())
+		switch s {
+		case syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			return
+		case syscall.SIGHUP:
+		default:
+		}
+	}
+}
---
+++ conf/config.go
@@ -0,0 +1,36 @@
+package conf
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/fsnotify/fsnotify"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Port int `json:"port"`
+}
+
+func ReadConfig() (*Config, error) {
+	v := viper.New()
+	v.SetConfigFile("config.json")
+
+	v.OnConfigChange(func(in fsnotify.Event) {
+		// 配置文件发生变更之后会调用的回调函数
+		fmt.Println("Config file changed:", in.Name)
+		log.Println(v.Get("port"))
+		// 在这里触发服务平滑更新
+	})
+	v.WatchConfig()
+
+	err := v.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	var c Config
+	err = v.Unmarshal(&c)
+	log.Println(c, v.Get("port"))
+	return &c, err
+}
